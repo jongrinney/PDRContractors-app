77e8b761f70a2155e1785d760a4d758a9cdb7545
silverhawk184 <silverhawk184@gmail.com>
2019-08-12 23:32:41 -0400

    lots of updates

--- www/login.js
+++ www/login.js
@@ -1,74 +1,73 @@
-window.doPasswordReset = function(){
+window.doPasswordReset = function() {
 	Swal({
 		title: 'Are you sure?',
-		text: "This will disable your current password.",
+		text: 'This will disable your current password.',
 		type: 'warning',
 		showCancelButton: true,
 		confirmButtonColor: '#3085d6',
 		cancelButtonColor: '#d33',
-		confirmButtonText: 'Yes, reset my password!'
+		confirmButtonText: 'Yes, reset my password!',
 	}).then((result) => {
 		if (result.value) {
-			setTimeout(function(){Swal.showLoading();},200);
-			$.post('/cms/modules/users/json/doPasswordReset.json.php',$('#loginForm').serialize(),function(){
+			setTimeout(function() {
+				Swal.showLoading();
+			}, 200);
+			$.post('/cms/modules/users/json/doPasswordReset.json.php', $('#loginForm').serialize(), function() {
 				Swal.hideLoading();
-				Swal(
-					'',
-					'If an account was found with that username, you will receive an email with a temporary password. Enter it in the password box to continue.',
-					'success'
-				)
+				Swal('', 'If an account was found with that username, you will receive an email with a temporary password. Enter it in the password box to continue.', 'success');
 			});
 		}
 	});
-}
+};
 
-
-$('#loginForm').submit(function(e){
-	if(P.sweetalert.isLoaded) Swal.showLoading();
+$('#loginForm').submit(function(e) {
+	if (P.sweetalert.isLoaded) Swal.showLoading();
 	e.preventDefault();
 	$('.menu-top .dropbtn').hide();
 	$('.menu-top a[class*="moduleButton_"]').hide();
-	$.post('https://pdrcontractors.com/cms/json/login.json.php',$(this).serialize(),function(data){
-		if(P.sweetalert.isLoaded) Swal.hideLoading();
-		if(data.message=='good'){
-			
+	$.post('https://pdrcontractors.com/cms/json/login.json.php', $(this).serialize(), function(data) {
+		if (P.sweetalert.isLoaded) Swal.hideLoading();
+		if (data.message == 'good') {
 			//store credentials
 			localStorage['username'] = $('#loginForm input[name="username"]').val();
 			localStorage['password'] = $('#loginForm input[name="password"]').val();
-			
+
 			//go to the next tab
-			$('main').removeClass('active').filter('#app').addClass('active');
+			$('main')
+				.removeClass('active')
+				.filter('#app')
+				.addClass('active');
 			$('#profile').addClass('active');
 			window.userId = data.access.id;
 			getVendorId();
 			initMap();
-		}
-		else if(data.message == 'doShowResetPassword'){
-			if($('#loginForm').children().length==3){
+		} else if (data.message == 'doShowResetPassword') {
+			if ($('#loginForm').children().length == 3) {
 				Swal({
 					title: 'Temporary Password accepted',
-					text: "Please enter a new permenant password below.",
-					type: 'success'
+					text: 'Please enter a new permanent password below.',
+					type: 'success',
 				});
-				$('#loginForm>label').last().after('<label>New Password<input name="new_password" type="password" autocomplete="current-password"></label>');
+				$('#loginForm>label')
+					.last()
+					.after('<label>New Password<input name="new_password" type="password" autocomplete="current-password"></label>');
 			}
-		}
-		else if(data.showForgotPasswordButton){
+		} else if (data.showForgotPasswordButton) {
 			Swal({
 				title: 'Incorrect Password',
-				text: "Please try again. If the problem persists, use the reset password button below.",
-				type: 'error'
+				text: 'Please try again. If the problem persists, use the reset password button below.',
+				type: 'error',
 			});
-			if($('#loginForm').children().length==3){
-				$('#loginForm>label').last().after('<input class="button" type="button" onclick="doPasswordReset()" value="Forgot Password" style="width:100%">');
+			if ($('#loginForm').children().length == 3) {
+				$('#loginForm>label')
+					.last()
+					.after('<input class="button" type="button" onclick="doPasswordReset()" value="Forgot Password" style="width:100%">');
 			}
-		}
-		else if($('#loginForm [name="username"]').val() != '')
-			alert(data.message);
+		} else if ($('#loginForm [name="username"]').val() != '') alert(data.message);
 	});
 	return false;
 });
-if(typeof localStorage['username'] === 'string'){
+if (typeof localStorage['username'] === 'string') {
 	$('#loginForm input[name="username"]').val(localStorage['username']);
 	$('#loginForm input[name="password"]').val(localStorage['password']);
 	$('#loginForm').trigger('submit');
--- www/pluginLoader.js
+++ www/pluginLoader.js
@@ -1,83 +1,82 @@
-window.P={};
-P.asyncLoad = function(pluginsList){
-	for(var i in pluginsList){
-		var 
-			namespace = pluginsList[i][0],
+window.P = {};
+P.asyncLoad = function(pluginsList) {
+	for (var i in pluginsList) {
+		var namespace = pluginsList[i][0],
 			src = pluginsList[i][1];
-		
-		this[namespace]={
+
+		this[namespace] = {
 			namespace: namespace,
 			src: src,
 			isLoaded: false,
-			loaded: function(){
-				console.log('P:loaded['+this.namespace+']',this.queue);
-				this.isLoaded=true;
+			loaded: function() {
+				console.log('P:loaded[' + this.namespace + ']', this.queue);
+				this.isLoaded = true;
 				this.run();
 			},
-			queue:[],
-			run: function(){
+			queue: [],
+			run: function() {
 				var task;
-				if(this.isLoaded && !this.isRunning){
+				if (this.isLoaded && !this.isRunning) {
 					this.isRunning = true;
-					while(this.queue.length>0){
+					while (this.queue.length > 0) {
 						task = this.queue.shift();
-						if(typeof task === 'function')
-							task();
+						if (typeof task === 'function') task();
 					}
 					this.isRunning = false;
 				}
 			},
 			isRunning: false,
-			addToQueue: function(what){
+			addToQueue: function(what) {
 				this.queue.push(what);
 				this.run();
 			},
-			ready: function(what){
+			ready: function(what) {
 				this.queue.push(what);
 				this.run();
 			},
-			init: function(defaultQueue){
-				if(typeof defaultQueue === 'undefined')
-					defaultQueue=[];
-				if(typeof defaultQueue === 'function')
-					defaultQueue=[defaultQueue];
+			init: function(defaultQueue) {
+				if (typeof defaultQueue === 'undefined') defaultQueue = [];
+				if (typeof defaultQueue === 'function') defaultQueue = [defaultQueue];
 				this.queue = this.queue.concat(defaultQueue);
-				
-				var 
-					s=document.createElement('script'), 
-					r=false, 
+
+				var s = document.createElement('script'),
+					r = false,
 					t;
 				s.type = 'text/javascript';
 				s.src = this.src;
-				s.setAttribute('data-namespace',this.namespace);
+				s.setAttribute('data-namespace', this.namespace);
 				//s.onload = P[this.namespace].loaded();
-				s.onload = function(){
+				s.onload = function() {
 					var namespace = this.getAttribute('data-namespace');
 					P[namespace].loaded();
 				};
 				t = document.getElementsByTagName('script')[0];
 				t.parentNode.insertBefore(s, t);
-			}
-		}
-	};
+			},
+		};
+	}
 };
 
 P.asyncLoad([
-	['jquery','jquery-3.2.1.min.js'],
-	['login','login.js'],
-	['forms','https://pdrcontractors.com/cms/modules/forms/js/forms.js'],
-	['sweetalert','https://pdrcontractors.com/cms/plugins/sweetalert2/dist/sweetalert2.all.min.js'],
-	['mapbox','https://api.mapbox.com/mapbox.js/v3.2.0/mapbox.js'],
-	['mapbox-cluster','https://pdrcontractors.com/js/Leaflet.markercluster-1.4.1/dist/leaflet.markercluster-src.js'],
-	['template7','template7.min.js'],
+	['jquery', 'jquery-3.2.1.min.js'],
+	['login', 'login.js'],
+	['forms', 'https://pdrcontractors.com/cms/modules/forms/js/forms.js'],
+	['sweetalert', 'https://pdrcontractors.com/cms/plugins/sweetalert2/dist/sweetalert2.all.min.js'],
+	['mapbox', 'https://api.mapbox.com/mapbox.js/v3.2.0/mapbox.js'],
+	['mapbox-cluster', 'https://pdrcontractors.com/js/Leaflet.markercluster-1.4.1/dist/leaflet.markercluster-src.js'],
+	['template7', 'template7.min.js'],
 ]);
 
 var cms = {};
 
-P.jquery.init(function(){
+P.jquery.init(function() {
+	$.ajaxSetup({
+		xhrFields: {
+			withCredentials: true,
+		},
+	});
 	P.login.init();
 	P.forms.init();
-	P.sweetalert.init()
-	P.template7.init()
+	P.sweetalert.init();
+	P.template7.init();
 });
-	
